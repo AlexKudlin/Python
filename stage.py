fb6bcf7bde115ad78b3f00151322618a2d60c44e
AlexKudlin <afrug@bk.ru>
2026-03-04 14:04:47 +0300

    Add files via upload

---
+++ stage.py
@@ -0,0 +1,10 @@
+stage_0 = input("Этап 0 (Dryopithecus): ")
+stage_1 = input("Этап 1 (Australopithecus afarensis): ")
+stage_2 = input("Этап 2 (Homo habilis): ")
+stage_3 = input("Этап 3 (Homo erectus): ")
+stage_4 = input("Этап 4 (Homo neanderthalensis): ")
+stage_5 = input("Этап 5 (Homo sapiens): ")
+stage_6 = input("Этап 6 (Homo sapiens sapiens): ")
+
+# Вывод всех стадий с символом перехода =>
+print(stage_0, stage_1, stage_2, stage_3, stage_4, stage_5, stage_6, sep=" => ")
